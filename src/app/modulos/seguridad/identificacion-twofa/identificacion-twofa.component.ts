2dd221234d211a8ac67014b9c4cc5acc93f0d849
LinaNieto <lina.nieto22243@ucaldas.edu.co>
2023-04-26 22:38:48 -0500

    Cambio de interfaz gráfica con el inicio de sesión video #40

--- src/app/modulos/seguridad/identificacion-twofa/identificacion-twofa.component.ts
+++ src/app/modulos/seguridad/identificacion-twofa/identificacion-twofa.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsuarioValidadoModel } from 'src/app/modelos/usuario.validado.model';
 import { SeguridadService } from 'src/app/servicios/seguridad.service';
 
 @Component({
@@ -14,7 +16,8 @@ export class IdentificacionTwofaComponent {
   fGroup: FormGroup = new FormGroup({})
   constructor(
     private servicioSeguridad: SeguridadService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private  router : Router
     ){
 
   }
@@ -24,6 +27,8 @@ export class IdentificacionTwofaComponent {
     if (datos != null){
       this.usuarioId = datos._id!;
       this.ConstruirFormulario();
+    }else{
+      this.router.navigate(['/seguridad/identificar-usuario'])
     }
   }
 
@@ -34,21 +39,21 @@ export class IdentificacionTwofaComponent {
   }
 
   ValidarCodigo2fa(){
-
     if ( this.fGroup.invalid){
       alert("Debe ingresar el codigo")
     }else{
       let codigo2fa = this.ObtenerFormGroup["codigo"].value;
       this.servicioSeguridad.ValidarCodigo2FA(this.usuarioId, codigo2fa).subscribe({
-        next: (datos:object)=>{
-          console.log(datos)
+        next: (datos:UsuarioValidadoModel)=>{
+          console.log(datos);
+          this.servicioSeguridad.AlmacenarDatosUsuarioValidado(datos);
+          this.router.navigate([""]);
         },
         error: (err) => {
           console.log(err);
         }
       });
     }
-    
   }
   get ObtenerFormGroup(){
     return this.fGroup.controls;
--- src/app/servicios/seguridad.service.ts
+++ src/app/servicios/seguridad.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { UsuarioModel } from '../modelos/usuario.model';
 import { HttpClient } from '@angular/common/http';
 import { ConfiguracionRutasBackend } from '../config/configuracion.rutas.backend';
+import { UsuarioValidadoModel } from '../modelos/usuario.validado.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SeguridadService {
   urlBase:string = ConfiguracionRutasBackend.urlSeguridad;
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) { 
+    this.ValidacionDeSesion();
+  }
 
   /**
    * identificar usuario
@@ -63,10 +66,46 @@ export class SeguridadService {
    * @returns 
    */
 
-  ValidarCodigo2FA(idUsuario:string, codigo:string): Observable<object>{
-    return this.http.post<UsuarioModel> (`${this.urlBase}verificar-2fa`,{
+  ValidarCodigo2FA(idUsuario:string, codigo:string): Observable<UsuarioValidadoModel>{
+    return this.http.post<UsuarioValidadoModel> (`${this.urlBase}verificar-2fa`,{
       usuarioId: idUsuario,
       codigo2fa: codigo
     });
+  }
+
+  /**
+   * Guardar en local storgae la informacion del usuario validado
+   * @param datos datos usuario validado
+   * @returns  respuesta
+   */
+
+  AlmacenarDatosUsuarioValidado(datos: UsuarioValidadoModel): boolean{
+    let datosLS = localStorage.getItem("datos-sesion");
+    if ( datosLS !=null){
+      return false;
+    }else{
+      let datosString = JSON.stringify(datos);
+      localStorage.setItem("datos-sesion", datosString)
+      return true;
+    }
+  }
+
+  /** Adimistracion de la sesion de usuario */
+
+  datosUsuarioValidado = new BehaviorSubject<UsuarioValidadoModel>(new UsuarioValidadoModel());
+  ObtenerDatosSesion(): Observable<UsuarioValidadoModel>{
+    return this.datosUsuarioValidado.asObservable();
+  }
+
+  ValidacionDeSesion(){
+    let ls = localStorage.getItem("datos-sesion");
+    if(ls){
+      let objUsuario = JSON.parse(ls);
+      this.ActualizarComportamientoUsuario(objUsuario)
+    }
+  }
+
+  ActualizarComportamientoUsuario(datos: UsuarioValidadoModel){
+    return this.datosUsuarioValidado.next(datos)
   }
 }
